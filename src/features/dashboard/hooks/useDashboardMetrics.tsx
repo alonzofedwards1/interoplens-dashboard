8f0ce8157740021857fec2d63ee70a57c06b4b3f
alonz <alonzoedwards184@gmail.com>
2026-01-11 16:06:36 -0600

    Small changes

--- src/features/dashboard/hooks/useDashboardMetrics.tsx
+++ src/features/dashboard/hooks/useDashboardMetrics.tsx
@@ -90,7 +90,7 @@ const buildAlertCards = (
             icon: <FaUsers className="text-blue-500 text-2xl" />,
             bgColor: 'bg-blue-100',
             textColor: 'text-blue-800',
-            route: '/pd-executions',
+            route: '/telemetry',
         },
         {
             title: 'Findings Detected',
